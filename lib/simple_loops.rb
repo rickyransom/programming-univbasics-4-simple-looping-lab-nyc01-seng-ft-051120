e579649095b55a212bd24150a38cc4c409ecd978
Jean-Richard Latortue <rickyransomco@gmail.com>
2020-04-29 13:59:59 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -29,7 +29,7 @@ end
 def return_string_array(array)
   new_array = []
   array.each do |item|
-    puts array.to_s
+    array.push(item.to_s)
   end
-  return new_array [""]
+  return new_array
 end
